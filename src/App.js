97bf12bb331444bc9d4c1bae0891e97116e2c412
daegeunkim <daegeunkim92@gmail.com>
2020-06-06 01:09:39 +0900

    express + react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [userName, setUserName] = useState('Default')
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api')
+    .then((res) => {
+      res.json().then((result) => {setUserName(result.userName)})
+    })
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Test value={userName}/>
       </header>
     </div>
   );
+}
+
+const Test = (props) => {
+  return (
+    <p>{props.value}</p>
+  )
 }
 
 export default App;
